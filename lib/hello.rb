8a07d13f30c8e95627f433739abc215c607b86d3
Lydell Gaines <dasnic70@gmail.com>
2020-02-20 04:19:28 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,4 +3,12 @@ def hello_t
 end
 
 # call your method here!
-
+def rb_block
+  puts 'Start'
+  # you can call the block using the yield keyword
+  yield
+  yield
+  puts 'End'
+end
+# Code blocks may appear only in the source adjacent to a method call
+rb_block {puts "We're in the block"}
